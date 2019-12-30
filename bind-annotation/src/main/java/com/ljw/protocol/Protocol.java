45e1c5a0859a8f93ec466ba902a818e6b00de9cb
laijianwei914 <877867730@qq.com>
2019-12-30 10:51:16 +0800

    Initial commit

---
+++ bind-annotation/src/main/java/com/ljw/protocol/Protocol.java
@@ -0,0 +1,134 @@
+package com.ljw.protocol;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Administrator on 2018/2/11 0011.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.CLASS)
+public @interface Protocol {
+    /**
+     * 协议字段角标
+     */
+    int index() default -1;
+
+    /**
+     * 协议字段的字节长度长度，如果该字段的字节长度是不确定的那么就不需要赋值，比如：数据域
+     */
+    int length() default Integer.MAX_VALUE;
+
+    /**
+     * 值为byte类型
+     * 协议字段的值，如果值是相对固定的就必须要赋值，如果不固定就不需要赋值，
+     * 比如：命令码，流水，数据域数据等，
+     * 这要里注意一下：
+     * 如果你的协议里面有数据长度这个字段，那么也必须要赋值；
+     * 如果你的协议数据长度只是计算数据域那么value = 0，
+     * 数据长度包含其他比如命令码等，那么value = 命令码等字节长度；数据域的长度会动态计算的
+     */
+    byte value() default -1;
+
+    /**
+     * 命令码长度
+     */
+    int commandLen() default -1;
+
+    /**
+     * 目标地址长度
+     */
+    int targetAddressLen() default -1;
+
+    /**
+     * 原地址长度
+     */
+    int rawAddressLen() default -1;
+
+    /**
+     * 协议版本长度
+     */
+    int protocolLen() default -1;
+
+    /**
+     * 异或字节长度
+     */
+    int oxrLen() default -1;
+
+    /**
+     * 数据长度
+     */
+    int dateLen() default -1;
+
+    /**
+     * 通信协议最短字节不包含数据域（
+     */
+    int minPackLen() default -1;
+
+    /**
+     * 帧头1
+     */
+    byte frameHeader() default -1;
+
+    /**
+     * 帧头2
+     */
+    byte frameHeader2() default -1;
+
+    /**
+     * 数据长度标识
+     */
+    int dataLenIndex() default Integer.MAX_VALUE;
+
+    /**
+     * 数据默认起始位置
+     */
+    int dataStartIndex() default Integer.MAX_VALUE;
+
+    /**
+     * 命令码开始标识
+     */
+    int commandStartIndex() default Integer.MAX_VALUE;
+
+    /**
+     * 流水号开始标识
+     */
+    int runningNumberIndex() default Integer.MAX_VALUE;
+
+    /**
+     * 安卓地址
+     */
+    byte androidAdress() default -1;
+
+    /**
+     * 主控板地址
+     */
+    byte hardwareAdress() default -1;
+
+    /**
+     * 版本协议
+     */
+    byte dealVersions() default -1;
+
+    /**
+     * 最小数据长度
+     */
+    int minDalaLen() default -1;
+
+    /**
+     * 帧头字节数
+     */
+    int frameHeaderCount() default -1;
+
+    /**
+     * 校验规则 0表示异或校验  1表示CRC16校验
+     */
+    int checkCodeRule() default -1;
+
+    /**
+     * 心跳命令
+     */
+    byte heartbeatCommand() default -1;
+}
